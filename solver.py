040269f07cb4a5566e9e6154431bc91ad92dec53
eashman123 <eashaank@gmail.com>
2019-04-17 17:13:43 -0700

    Added some more structure

--- solver.py
+++ solver.py
@@ -1,4 +1,35 @@
-# Put your solution here.
+import networkx as nx
+import random
 
 def solve(client):
-    pass
+    client.end()
+    client.start()
+    #Our code begin
+
+
+    loc = Locate(client)
+    loc.find()
+
+    rem = Move(client)
+    rem.move()
+
+
+    #Our code end
+    client.end()
+
+class Locate:
+    def __init__(self, client):
+        self.all_students = list(range(1, client.students + 1))
+        self.num_bots = client.bots
+        self.vertices = list(range(1, client.home)) + list(range(client.home + 1, client.v + 1))
+
+    def find(self): #Finds location of all bots
+        pass
+
+class Move:
+    def __init__(self, client):
+        self.home = client.home
+        self.vertices = list(range(1, client.home)) + list(range(client.home + 1, client.v + 1))
+
+    def move(self): #moves all bots to home
+        pass
