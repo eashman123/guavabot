9eb26718ebc3ed4085736951b75481645e5c1ba6
eashman123 <eashaank@gmail.com>
2019-04-21 16:41:40 -0700

    Added more functionality to autorun

--- autorun.py
+++ autorun.py
@@ -1,15 +1,22 @@
 import os, sys
+from statistics import mean, stdev
 
-def test(n):
-    sum=0
+def test(n=30, teval=False):
+    scores=[]
     for i in range(n):
         score = float(os.popen("python client.py --solver solver").readlines()[-1].split()[-1])
-        sum+=score
         print(score)
-    print("Average: " + str(sum/n))
+        scores.append(score)
+    evaluated_scores = sorted(scores)[int(len(scores)*.12):]
+    if teval:
+        scores=evaluated_scores
+    print("Average: " + str(mean(scores)))
+    print("Standard Deviation: " + str(stdev(scores)))
 
 if __name__=="__main__":
     arguments = sys.argv
-    assert len(arguments)==2, "Must only include 1 argument"
-    assert isinstance(int(arguments[1]), int), "Must only be a number"
-    test(int(arguments[1]))
+    if len(arguments)==1:
+        test(30, True)
+    else:
+        assert len(arguments)==2, "Must only include 1 argument"
+        test(int(arguments[1]))
