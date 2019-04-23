cf142af4a4cadb296ae5d6ae06d4b46ef3d57851
eashman123 <eashaank@gmail.com>
2019-04-23 04:10:13 -0700

    Create my_server.py

---
+++ my_server.py
@@ -0,0 +1,12 @@
+import local_server, argparse
+
+if __name__=="__main__":
+    instances = ["toronto_12_17"]
+    for inst in instances:
+        parser = argparse.ArgumentParser(description='Guavabot local server: move all bots home!')
+        parser.add_argument('--instance', dest='instance_name', default=inst,
+                        help='The local instance to always serve.')
+        args = parser.parse_args()
+
+        instance_name = args.instance_name
+
